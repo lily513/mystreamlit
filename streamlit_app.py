09c159bcab5e48ee4882a952f33da706542b36d4
lily513 <143357099+lily513@users.noreply.github.com>
2023-09-09 11:51:46 -0400

    AI KEY

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
 
 
 """
@@ -15,11 +12,42 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 hello this is lily 
 """
+import openai
 import streamlit as st
-import numpy as np
 
-message = st.chat_message("assistant")
-message.write("Hello human")
-message.bar_chart(np.random.randn(30, 3))
+st.title("ChatGPT-like clone")
+
+openai.api_key = "ii7YM3eQKRNrl3QulpNfP6ys9AXA3diojz2CS0E"
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in openai.ChatCompletion.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 
