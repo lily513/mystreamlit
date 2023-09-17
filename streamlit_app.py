136fc184b8afaf5fddd8a2df5845204b1c48fad3
lily513 <143357099+lily513@users.noreply.github.com>
2023-09-17 14:36:05 -0400

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,10 +21,10 @@ openai.api_key = st.secrets["OPENAI_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
-
+    st.write(openai.api_key)
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
+ 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
