8e490eb25aa7dfa777d035a2ce3f632b6b0a01fd
lily513 <143357099+lily513@users.noreply.github.com>
2023-09-17 14:26:17 -0400

    more ai key stuff

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ import streamlit as st
 
 st.title("ChatGPT-like clone")
 
-openai.api_key = "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"
+openai.api_key = st.secrets["OPENAI_KEY"]
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
